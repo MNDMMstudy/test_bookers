664a5f74c9cac9659411720666dfd8d53c111ec4
MNDMMstudy <107236809+MNDMMstudy@users.noreply.github.com>
2022-07-09 10:59:37 +0000

    index作成

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'lists/new'
+  get 'books' => 'lists#index'
+  post 'lists' => 'lists#create'
+  get 'lists/show'
+  get 'lists/edit'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/' => 'homes#top'
+end
