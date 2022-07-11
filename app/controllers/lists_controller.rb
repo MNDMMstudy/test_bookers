c972cc0d8273124fa4d4335f3d5bb859b1ef3b2f
MNDMMstudy <107236809+MNDMMstudy@users.noreply.github.com>
2022-07-11 03:07:56 +0000

    create_destroy

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,7 +1,4 @@
 class ListsController < ApplicationController
-  def new
-
-  end
 
   def create
     list = List.new(list_params)
@@ -21,11 +18,17 @@ class ListsController < ApplicationController
   def edit
     @list = List.find(params[:id])
   end
-  
+
   def update
     list = List.find(params[:id])
     list.update(list_params)
     redirect_to list_path(list.id)
+  end
+
+  def destroy
+   list = List.find(params[:id])
+   list.destroy
+   redirect_to '/books'
   end
 
   private
