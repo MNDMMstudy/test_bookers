39d169388156150c6460b2b11e40b82d2de6d43d
MNDMMstudy <107236809+MNDMMstudy@users.noreply.github.com>
2022-07-11 05:49:59 +0000

    call

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,9 +1,14 @@
 class ListsController < ApplicationController
 
   def create
-    list = List.new(list_params)
-    list.save
-    redirect_to list_path(list.id)
+    flash[:notice] = "successfully"
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list.id)
+    else
+      @lists = List.all
+      render :index
+    end
   end
 
   def index
@@ -16,16 +21,19 @@ class ListsController < ApplicationController
   end
 
   def edit
+    flash[:notice] = "successfully"
     @list = List.find(params[:id])
   end
 
   def update
+    flash[:notice] = "successfully"
     list = List.find(params[:id])
     list.update(list_params)
     redirect_to list_path(list.id)
   end
 
   def destroy
+    flash[:notice] = "successfully"
    list = List.find(params[:id])
    list.destroy
    redirect_to '/books'
